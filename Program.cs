92545b087554395e6457f822aec8bb790b52d903
Supuni Eleesha Randeniya <87631717+Radser2001@users.noreply.github.com>
2024-10-17 18:49:35 +0530

    Register the MovieService

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using FilmVault.Data;
+using FilmVault.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSqlite<MovieContext>("Data Source=FilmVault.db");
+
+builder.Services.AddScoped<MovieService>();
 
 var app = builder.Build();
 
